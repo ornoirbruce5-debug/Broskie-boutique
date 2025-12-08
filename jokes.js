f2c6ff55b74f655ae1c85171bd6923ac06b0385b
Intimatelures <ornoirbruce5@gmail.com>
2025-12-08 07:30:45 +0200

    Update jokes.js

--- jokes.js
+++ jokes.js
@@ -8,8 +8,24 @@
 const JOKES = [
   {
     id: "j-001",
-    text: "Umwana arabaza: 'Papa, internet ivuga iki?' Papa ati: 'Ni ahantu abantu bakina bakavuga byinshi, ariko bakibagirwa gusoma inyandiko.'",
-    image: "jokes/internet.png"
+    text: "  Umugabo yakubise umugore wiwe ahakwa kumwica  maze baramufunga .
+Haheze imisi bamujana muri sentare kugira ahanirwe icaha co kugerageza kwica👇👇
+
+Umucamanza: wa mugabo we, wagirizwa icaha co gushaka kwica, uravyemera?
+
+Umugabo: ndavyemera😎
+
+Umucamanza: none kubera iki wamuhondesheje intebe?🤔
+Umugabo: kuko vyananiye guterura imeza 😭🤣🤣🤣🤣
+
+AGAKURU KA BONUS
+
+Umwigisha yariko arabwira abanyeshure kwigana umwete, arababwira ati ni mwigane umwete mumare amashure muje kurondera amafaranga.
+Mumenye ko amafaranga atamera ku biti.
+Akana kamwe karahaguruka kati: nimba amafaranga atamera ku biti, kubera iki amabanki agira amashami🤔😭🤣🤣🤣
+
+Ndiyaranja n'ubwo har'aho ntagutwenza ariko mba nifuza ko uzinduka canke wirirwa unezerewe rero muri bwa bumwe fyonda aha 👉 Buja Entertainment and Tech urabe ahanditse suivre, follow canke gukurikira bivanye n'ururimi Facebook yawe irimwo uhafyonde ugume witwengera ❤️ '",
+    image: "jokes/joke-1.jpg"
   },
   {
     id: "j-002",
